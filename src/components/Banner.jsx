3a106fbfaf535b7dd1c6bf1bf6a7d809b98c3460
khaled6013 <kmd601384@gmail.com>
2026-01-01 17:32:01 +0600

    contextApi connect

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ApiData } from './ContextApi'
 
 const Banner = () => {
+  let data = useContext(ApiData)
+  console.log(data);
+  
+  
   return (
     <div className='py-20 bg-red-400'>Banner</div>
   )
