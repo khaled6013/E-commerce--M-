5d8cdca36ed1667bb3ab4cb1149a9e0d2c781c2d
khaled6013 <kmd601384@gmail.com>
2026-01-24 20:05:03 +0600

    pages 404 setup

--- src/components/PagesM.jsx
+++ src/components/PagesM.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import errorImg from '../assets/image/404.png' 
 
 const PagesM = () => {
   return (
-    <>
-    <section className='lg:py-20 py-5'>
-        <div className="lg:w-9/12 w-11/12 mx-auto">
-            <h2 className='font-josefin text-[34px] text-[#0D0E43] font-bold text-center'>404 Not Found</h2>
+    <section className='lg:py-20 py-12 bg-white'>
+        <div className="lg:w-9/12 w-11/12 mx-auto flex flex-col items-center">
+            <div className="mb-10">
+                <img src={errorImg} alt="404 Not Found" className="max-w-full h-auto" />
+            </div>
+
+            <div className="text-center">
+                <Link 
+                    to="/" 
+                    className="bg-[#FB2E86] text-white font-josefin text-[16px] px-8 py-3 rounded-sm hover:bg-[#F94C9B] transition-all duration-300 shadow-md inline-block"
+                >
+                    Back To Home
+                </Link>
+            </div>
         </div>
     </section>
-    </>
   )
 }
 
